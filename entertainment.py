90ef84493e7fd5a40889c454c2fcddd48b090d37
Nehal Kumar Singh <nehaljohn91@gmail.com>
2019-07-22 23:48:38 +0530

    entertainment class added

---
+++ entertainment.py
@@ -0,0 +1,8 @@
+import media
+
+iron_man_01=media.Movie("IRON MAN",
+                        "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+                        "https://en.wikipedia.org/wiki/File:Ironmanposter.JPG",
+                        "https://www.youtube.com/watch?v=8ugaeA-nMTc")
+
+print(iron_man_01.storyline)
