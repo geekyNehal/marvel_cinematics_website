3f96da7d5d3ffc0a7bbbccfa6fbf9a6172acedd4
Nehal Kumar Singh <nehaljohn91@gmail.com>
2019-07-23 03:04:29 +0530

    added movie attributes

--- entertainment.py
+++ entertainment.py
@@ -5,4 +5,10 @@ iron_man_01=media.Movie("IRON MAN",
                         "https://en.wikipedia.org/wiki/File:Ironmanposter.JPG",
                         "https://www.youtube.com/watch?v=8ugaeA-nMTc")
 
-print(iron_man_01.storyline)
+#print(iron_man_01.storyline)
+
+incredible_hulk=media.Movie("THE INCREDIBLE HULK",
+                            "Bruce Banner, a scientist on the run from the U.S. Government, must find a cure for the monster he turns into, whenever he loses his temper.",
+                            "https://en.wikipedia.org/wiki/File:The_Incredible_Hulk_poster.jpg",
+                            "https://www.youtube.com/watch?v=xbqNb2PFKKA")
+print(incredible_hulk.storyline)
