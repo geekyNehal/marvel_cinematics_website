4ede3830f58e3444081b6780583cca6b9c2b88fd
Nehal Kumar Singh <nehaljohn91@gmail.com>
2019-07-22 23:25:42 +0530

    movie init function added

---
+++ media.py
@@ -0,0 +1,7 @@
+class Movie():
+    def __init__(self,movie_title,movie_storyline,poster_img,trailer_youtube):
+
+        self.title=movie_title
+        self.storyline=movie_storyline
+        self.poster_img_url=poster_img
+        self.trailer_youtube_url=trailer_youtube
