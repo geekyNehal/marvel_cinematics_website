44bb3fdb21ee8f29305047eec3a22312e23cc26f
Nehal Kumar Singh <nehaljohn91@gmail.com>
2019-07-23 21:16:39 +0530

    website file added

--- entertainment.py
+++ entertainment.py
@@ -1,14 +1,19 @@
+import fresh_tomatoes
 import media
 
 iron_man_01=media.Movie("IRON MAN",
                         "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
-                        "https://en.wikipedia.org/wiki/File:Ironmanposter.JPG",
+                        "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
                         "https://www.youtube.com/watch?v=8ugaeA-nMTc")
 
 #print(iron_man_01.storyline)
 
 incredible_hulk=media.Movie("THE INCREDIBLE HULK",
                             "Bruce Banner, a scientist on the run from the U.S. Government, must find a cure for the monster he turns into, whenever he loses his temper.",
-                            "https://en.wikipedia.org/wiki/File:The_Incredible_Hulk_poster.jpg",
+                            "https://upload.wikimedia.org/wikipedia/en/8/88/The_Incredible_Hulk_poster.jpg",
                             "https://www.youtube.com/watch?v=xbqNb2PFKKA")
-print(incredible_hulk.storyline)
+#print(incredible_hulk.storyline)
+#incredible_hulk.show_trailer()
+
+movies=[iron_man_01,incredible_hulk]
+fresh_tomatoes.open_movies_page(movies)
